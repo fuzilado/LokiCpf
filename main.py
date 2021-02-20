3fba54c50957d69d202c28ff1ef7d8e03e2d10bf
Near Shelby <74830167+nearshelby-yt@users.noreply.github.com>
2021-02-20 16:13:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,39 @@ def main():
     cpf = input("\033[1;34m>>> \033[1;36m")
     print("\n\033[1;92mConsultando... Aquarde\n")
 
-    request = requests.get('http://104.41.5.41:12345/cpf.php?lista={}'.format(cpf))
+    request = requests.get('http://191.235.78.142/cpf.php?cpf={}'.format(cpf))
 
     address_data = request.json()
 
-    if 'erro' not in address_data:
+    if 'status' not in address_data:
         os.system('cls' if os.name == 'nt' else 'clear')
         print()
         print('\033[1;32m>\033[1;35m>\033[1;32m>\033[1;34m ENCONTRADO! \033[1;32m<\033[1;35m<\033[1;32m<')
         print()
-        print('\033[1;36mNome\033[1;33m:\033[1;92m {}'.format(address_data['Nome']))
-        print('\033[1;36mNascimento\033[1;33m:\033[1;92m {}'.format(address_data['Nacimento']))
-        print('\033[1;36mRua\033[1;33m:\033[1;92m {}'.format(address_data['Nologradouro']))
-        print('\033[1;36mCasa\033[1;33m:\033[1;92m {}'.format(address_data['Nrlogradouro']))
-        print('\033[1;36mBairro\033[1;33m:\033[1;92m {}'.format(address_data['Bairro']))
-        print('\033[1;36mCep\033[1;33m:\033[1;92m {}'.format(address_data['Cep']))
-        print('\033[1;36mCidade\033[1;33m:\033[1;92m {}'.format(address_data['Municipio'])) 
-        print('\033[1;36mEstado\033[1;33m:\033[1;92m {}'.format(address_data['Estado']))
-        print('\033[1;36mMae\033[1;33m:\033[1;92m {}'.format(address_data['Mae']))
-        print('\033[1;36mComplemento\033[1;33m:\033[1;92m {}'.format(address_data['Complemento']))
-        print()
+        print('\033[1;36mNome\033[1;33m:\033[1;92m {}'.format(address_data['nome']))
+        print('\033[1;36mNome da mae\033[1;33m:\033[1;92m {}'.format(address_data['nomeMae']))
+        print('\033[1;36mNome do pai\033[1;33m:\033[1;92m {}'.format(address_data['nomePai']))
+        print('\033[1;36mSexo\033[1;33m:\033[1;92m {}'.format(address_data['sexoDescricao']))
+        print('\033[1;36mCor\033[1;33m:\033[1;92m {}'.format(address_data['racaCorDescricao']))
+        print('\033[1;36mTipo Sanguineo\033[1;33m:\033[1;92m {}'.format(address_data['tipoSanguineo']))
+        print('\033[1;36mData Nascimento\033[1;33m:\033[1;92m {}'.format(address_data['dataNascimento']))
+        print('\033[1;36mNacionalidade\033[1;33m:\033[1;92m {}'.format(address_data['nacionalidade']))
+        print('\033[1;36mPais Nascimento\033[1;33m:\033[1;92m {}'.format(address_data['paisNascimento']))
+        print('\033[1;36mMunicipio Nascimento\033[1;33m:\033[1;92m {}'.format(address_data['municipioNascimentoSemUF']))
+        print('\033[1;36mEmail Principal\033[1;33m:\033[1;92m {}'.format(address_data['emailPrincipalValidado']))
+        print('\033[1;36mEndereco Codigo\033[1;33m:\033[1;92m {}'.format(address_data['enderecoCodigo']))
+        print('\033[1;36mRua\033[1;33m:\033[1;92m {}'.format(address_data['enderecoLogradouro']))
+        print('\033[1;36mCasa\033[1;33m:\033[1;92m {}'.format(address_data['enderecoNumero']))
+        print('\033[1;36mComplemento\033[1;33m:\033[1;92m {}'.format(address_data['enderecoComplemento']))
+        print('\033[1;36mBairro\033[1;33m:\033[1;92m {}'.format(address_data['enderecoBairro']))
+        print('\033[1;36mCep\033[1;33m:\033[1;92m {}'.format(address_data['enderecoCep']))
+        print('\033[1;36mRg\033[1;33m:\033[1;92m {}'.format(address_data['rgNumero']))
+        print('\033[1;36mListaCns\033[1;33m:\033[1;92m {}'.format(address_data['listaCns']))
 
-    opt = input('\033[1;33mDeseja fazer uma nova consulta? \033[1;32ms\033[1;33m/\033[1;31mn\033[1;33m:\033[1;36m ')
+    else:
+        print('\033[1;31mNão encontrado')
+
+    opt = input('\n\033[1;33mDeseja fazer uma nova consulta? \033[1;32ms\033[1;33m/\033[1;31mn\033[1;33m:\033[1;36m ')
     if opt == 's':
         main()
 
